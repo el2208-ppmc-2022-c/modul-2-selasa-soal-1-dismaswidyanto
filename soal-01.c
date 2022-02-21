27f7bb93b9e3f3066e458d6364ff2bdde5c24a53
dismaswidyanto <63232560+dismaswidyanto@users.noreply.github.com>
2022-02-21 12:32:39 +0700

    Update soal-01.c

--- soal-01.c
+++ soal-01.c
@@ -10,11 +10,77 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#define MAX_STRING 200
+
+char encryptedText[MAX_STRING];
+char plainText[MAX_STRING];
+char key[MAX_STRING];
+
+/*
+    Deskripsi fungsi: mengambil data pesan terenskripsi dan key dan menyimpannya pada variabel encryptedText dan key
+*/
+void getData(){
+    // Deklarasi pointer file
+    FILE *filePointer ;
+     
+    // Deklarasi variabel
+    char filename[MAX_STRING];
+    char buffer[MAX_STRING];
+
+    // Input nama file
+    printf("Masukkan nama file: ");
+    scanf("%s", filename);
+ 
+    // Membuka file untuk dibaca
+    filePointer = fopen(filename, "r") ;
+     
+    // Mengecek apakah file kosong atau tidak
+    if ( filePointer == NULL )
+    {
+        // Apabila file kosong
+        printf( "File gagal dibuka." ) ;
+    }
+    else
+    {
+        // Apabila file berisi data
+        // Mengambil data key
+        fgets (encryptedText, MAX_STRING, filePointer);
+
+        fgets (key, MAX_STRING, filePointer); 
+
+        // Menutup file
+        fclose(filePointer);
+    }
+
+    return;
+}
+
+/*
+    Deskripsi fungsi: mendekripsi pesan dan menyimpannya pada variabel plainText
+*/
+void xorDecrypt(char encrypt[MAX_STRING], char key[MAX_STRING]){
+    int i = 0;
+    int lenKey;
+    lenKey = strlen(key);
+
+    while(encryptedText[i] != '\0' && encryptedText[i] != '\n'){
+        plainText[i] = encryptedText[i] ^ key[i%lenKey];
+        i++;
+    }
+
+
+    plainText[i] = '\0';
+
+    return;
+}
 
 int main(){
-	
-	//template output
-	printf("%s",...);
-	
-	return 0;
+    getData();
+
+    xorDecrypt(encryptedText, key);
+
+    printf("%s", plainText);
+    return 0;
 }
